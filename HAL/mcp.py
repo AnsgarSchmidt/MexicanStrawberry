0d2bd2f33b47057fcd8bce52c99e418f8ebdd346
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2016-08-28 19:18:36 +0200

    Adding WeatherData

--- HAL/mcp.py
+++ HAL/mcp.py
@@ -3,6 +3,7 @@ from IBMConnector import IBMConnector
 from Dallas       import Dallas
 from DHT          import DHT
 from SystemData   import SystemData
+from Weather      import Weather
 
 def commandCallback(cmd):
         print("Command received: %s" % cmd.command)
@@ -42,21 +43,35 @@ if __name__ == '__main__':
     print "MS HAL start"
 
     iotfClient       = IBMConnector(commandCallback)
+    time.sleep(1)
     waterTemperature = Dallas()
+    time.sleep(1)
     airInside        = DHT(23)
+    time.sleep(1)
     airOutside       = DHT(24)
+    time.sleep(1)
     systemData       = SystemData()
+    time.sleep(1)
+    weather          = Weather()
+    time.sleep(1)
 
     while True:
         m = {}
-        m['Watertemperature']   = waterTemperature.getWaterTemp()
-        m['InsideHumidity']     = airInside.getHumidity()
-        m['InsideTemperature']  = airInside.getTemperature()
-        m['OutsideHumidity']    = airOutside.getHumidity()
-        m['OutsideTemperature'] = airOutside.getTemperature()
-        m['CPUTemperature']     = systemData.getCPUTemp()
-        m['GPUTemperature']     = systemData.getGPUTemp()
-        m['CPUUsage']           = systemData.getCPUuse()
-        m['Loadlevel']          = systemData.getLoadLevel()
+        m['Watertemperature']        = waterTemperature.getWaterTemp()
+        m['InsideHumidity']          = airInside.getHumidity()
+        m['InsideTemperature']       = airInside.getTemperature()
+        m['OutsideHumidity']         = airOutside.getHumidity()
+        m['OutsideTemperature']      = airOutside.getTemperature()
+        m['CPUTemperature']          = systemData.getCPUTemp()
+        m['GPUTemperature']          = systemData.getGPUTemp()
+        m['CPUUsage']                = systemData.getCPUuse()
+        m['Loadlevel']               = systemData.getLoadLevel()
+        m['WeatherHumidity']         = weather.getHumidity()
+        m['WeatherPressure']         = weather.getPressure()
+        m['WeatherPressureTrent']    = weather.getPressureTrent()
+        m['WeatherUV']               = weather.getUV()
+        m['WeatherPreciptionTotal']  = weather.getPrecipTotal()
+        m['WeatherPreciptionHourly'] = weather.getPrecipHrly()
+
         iotfClient.pushDataToIBM(m)
         time.sleep(1)
